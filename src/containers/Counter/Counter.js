8a7070d5bc31e3ffa50ed6dd875487cf29e83403
Ali Maqsood <ali-maqsood@hotmail.com>
2018-05-18 21:26:03 -0400

    Modularized code, added container structure

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
-import * as actions from '../actions';
+import './Counter.css';
+import * as actions from '../../actions';
 import { connect } from 'react-redux';
 
 class App extends Component {
@@ -21,10 +21,8 @@ class App extends Component {
           <h1 className="App-title">Redux inc/dec challenge</h1>
           <p className="count">{this.props.count}</p>
         </header>
-        <div className="buttons">
-          <button onClick={() => this.increment(count)}>Increment</button>
-          <button onClick={() => this.decrement(count)}>Decrement</button>
-        </div>
+        <button onClick={() => this.increment(count)}>Increment</button>
+        <button onClick={() => this.decrement(count)}>Decrement</button>
       </div>
     );
   }
